dbfad19b0a54eb03e2d4e3ce209a97d4488a55a8
Robin Lovelace <rob00x@gmail.com>
2021-09-29 12:39:50 +0100

    Update qgis scripts - needs gpg installed

--- scripts/install_qgis.sh
+++ scripts/install_qgis.sh
@@ -1,4 +1,5 @@
 # source: https://github.com/paleolimbot/qgisprocess/blob/master/.github/workflows/R-CMD-check.yaml
+apt install -y gpg software-properties-common
 wget -qO - https://qgis.org/downloads/qgis-2021.gpg.key | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/qgis-archive.gpg --import || true
 chmod a+r /etc/apt/trusted.gpg.d/qgis-archive.gpg
 add-apt-repository "deb https://qgis.org/ubuntu `lsb_release -c -s` main"
