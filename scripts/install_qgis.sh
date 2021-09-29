4d41827a0f90187a137a8fdcb677cb1304fd7dea
Robin Lovelace <rob00x@gmail.com>
2021-09-29 23:34:14 +0100

    Fix QGIS installation issues for #9

--- scripts/install_qgis.sh
+++
@@ -1,7 +0,0 @@
-# source: https://github.com/paleolimbot/qgisprocess/blob/master/.github/workflows/R-CMD-check.yaml
-apt install -y gpg software-properties-common
-wget -qO - https://qgis.org/downloads/qgis-2021.gpg.key | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/qgis-archive.gpg --import || true
-chmod a+r /etc/apt/trusted.gpg.d/qgis-archive.gpg
-add-apt-repository "deb https://qgis.org/ubuntu `lsb_release -c -s` main"
-apt-get update
-apt-get install -y qgis qgis-plugin-grass saga
